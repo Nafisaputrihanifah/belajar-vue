029dcd3910c4f5d80c7467ef63f45c37e552f3ca
davin <davin_257@smkassalaambandung.sch.id>
2023-07-20 15:50:52 +0700

    belajar-vue

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import {ref} from 'vue'
+
+const msg = ("Hello Akatsuki");
+const rawHtml = ("Pain");
+</script>
+
+<template>
+  <div  id="app">
+    
+    <h1>{{ msg }}</h1>
+
+    
+    <p> {{ rawHtml }}</p>
+    <p>
+      <span v-html="rawHtml"></span>
+    </p>
+    <span style="color: rgb(255, 0, 0)">akatsuki</span>
+    <button :disabled="isButtonDisabled">KLIK</button>
+
+  </div>
+
+ 
+ 
+</template>  
